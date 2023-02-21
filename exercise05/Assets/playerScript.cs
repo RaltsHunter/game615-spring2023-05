1a73245144f97b3e7a047557d6cb25ff8a7e370e
RaltsHunter <117216708+RaltsHunter@users.noreply.github.com>
2023-02-21 14:10:06 -0600

    particle system stuff

--- exercise05/Assets/playerScript.cs
+++ exercise05/Assets/playerScript.cs
@@ -7,6 +7,7 @@ public class playerScript : MonoBehaviour
 {
     public float BBQBottles = 0;
     public TMP_Text bbqCount;
+    public ParticleSystem party;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +27,7 @@ public class playerScript : MonoBehaviour
             Destroy(other.gameObject);
             BBQBottles = BBQBottles + 1;
             bbqCount.text = BBQBottles.ToString();
+            ParticleSystem partay = Instantiate(party);
 
         }
     }
